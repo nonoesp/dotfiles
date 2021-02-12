8ffdbfd07c9e2e6ae8ff42e2fb74860acd2dc45a
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-02-13 00:23:04 +0100

    Add duti.

---
+++ duti/install.sh
@@ -0,0 +1,27 @@
+# https://github.com/moretension/duti/
+
+echo "› duti"
+
+###############################################################################
+# Visual Studio Code                                                          #
+###############################################################################
+
+# Bash
+duti -s com.microsoft.VSCode sh all
+# XML
+duti -s com.microsoft.VSCode xml all
+# CSV
+duti -s com.microsoft.VSCode csv all
+# JSON
+duti -s com.microsoft.VSCode json all
+# HTML
+duti -s com.microsoft.VSCode public.html all
+# PHP
+duti -s com.microsoft.VSCode php all
+
+###############################################################################
+# iA Writer                                                                   #
+###############################################################################
+
+# Markdown
+duti -s pro.writer.mac md all
