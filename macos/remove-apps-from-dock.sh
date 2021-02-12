37b837f3d299ac3c01d0f90c7e421e565aacde3b
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-02-13 00:22:26 +0100

    Add remove apps from Dock.

---
+++ macos/remove-apps-from-dock.sh
@@ -0,0 +1,37 @@
+###############################################################################
+# Remove apps from Dock                                                       #
+###############################################################################
+
+if [ "$(uname)" == "Darwin" ]; then
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+for app in "App Store" \
+	"TV" \
+	"Contacts" \
+	"Calendar" \
+	"Pages" \
+	"Keynote" \
+	"News" \
+    "Music" \
+    "Podcasts" \
+    "System Preferences" \
+    "Downloads" \
+    "Notes" \
+    "Messages" \
+    "Reminders" \
+    "Photos" \
+    "Maps" \
+    "Mail" \
+    "FaceTime" \
+    "Safari" \
+    "Launchpad" \
+	"Numbers"; do
+	dockutil --remove "${app}" &> /dev/null
+done
+
+fi
