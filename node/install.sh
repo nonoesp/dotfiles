de3adbca90a8da64c8ce4438bf725264957e7a4f
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-02-13 00:22:04 +0100

    Edit node script.

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,18 @@
+# TODO: loop through list or use package.json?
+echo "› npm"
+
 if test ! $(which spoof)
 then
   if test $(which npm)
   then
     sudo npm install spoof -g
+  fi
+fi
+
+if test ! $(which pm2)
+then
+  if test $(which npm)
+  then
+    sudo npm install pm2 -g
   fi
 fi
