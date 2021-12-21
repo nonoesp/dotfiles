69e9a516ee4dc4f34b945b1d3210d3c037fdad2a
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-12-21 12:23:45 +0100

    Update.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,14 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Google Cloud SDK - gcloud command
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+
+# Google Cloud SDK - command completion for gcloud
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
+# NVM 2021.07.21
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
