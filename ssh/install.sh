936b522138b8f164785a85962d3c01dbcc8990fd
Nono Martínez Alonso <mundowarezweb@gmail.com>
2022-09-09 15:57:53 +0200

    Update SSH from rsa to ed25519.

--- ssh/install.sh
+++ ssh/install.sh
@@ -8,7 +8,7 @@
 
 echo "› ssh"
 
-KEY_PATH=~/.ssh/id_rsa
+KEY_PATH=~/.ssh/id_ed25519
 
 if ! test -f "$KEY_PATH"; then
 
@@ -18,20 +18,20 @@ if ! test -f "$KEY_PATH"; then
 		read -p "  Enter your e-mail: " SSH_email
 		echo ""
 		echo "  [SSH] Creating SSH key"
-		SSH-keygen -f $KEY_PATH -t rsa -b 4096 -C $SSH_email
+		SSH-keygen -f $KEY_PATH -t ed25519 -b 4096 -C $SSH_email
+		echo ""
+
+		echo "  [SSH] Adding SSH key to SSH-agent"
+		eval "$(SSH-agent -s)"
+		SSH-add $KEY_PATH
+		echo ""
+
+		echo "  [SSH] Copying SSH key to clipboard"
+		pbcopy < "$KEY_PATH.pub"
+
+		echo "  [SSH] Done"
+		echo ""
 	fi
-	echo ""
-
-	echo "  [SSH] Adding SSH key to SSH-agent"
-	eval "$(SSH-agent -s)"
-	SSH-add $KEY_PATH
-	echo ""
-
-	echo "  [SSH] Copying SSH key to pasteboard"
-	pbcopy < "$KEY_PATH.pub"
-
-	echo "  [SSH] Done"
-	echo ""
 
 else
 
