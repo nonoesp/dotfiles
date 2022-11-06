d44ad97fc80bfb60ee399c82c90ec1ec66d2bfbb
Nono Martínez Alonso <mundowarezweb@gmail.com>
2022-11-06 22:13:39 +0100

    Display YouTube Url in lieue of machine name.

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -10,9 +10,11 @@ function title() {
   case $TERM in
   screen)
     print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
+    # print -Pn "\ek:\e\\" # screen title (in ^A")
     ;;
   xterm*|rxvt)
-    print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
+    # print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
+    print -Pn "\e]2;youtube.com/@NonoMartinezAlonso\a" # plain xterm title ($3 for pwd)
     ;;
   esac
 }
