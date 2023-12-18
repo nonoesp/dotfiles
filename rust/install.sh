1212d3fbfaf2330f93fd3eb2d45f3dcee3cb11ce
Nono Martínez Alonso <mundowarezweb@gmail.com>
2023-12-18 19:05:26 +0100

    Add rust install and path.

---
+++ rust/install.sh
@@ -0,0 +1,12 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Verify versions.
+# rustc --version
+# cargo --version
+# rustup --version
+
+# Update rustup.
+# rustup update
+
+# Uninstall rustup.
+# rustup self uninstall
