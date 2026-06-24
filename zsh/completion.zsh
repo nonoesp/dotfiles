f6b564de5adf70015aea7567f47f2cdac60296f3
Nono Martínez Alonso <mundowarezweb@gmail.com>
2026-06-24 17:48:10 +0200

    Remove NVM from completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -9,8 +9,3 @@ zstyle ':completion:*' insert-tab pending
 
 # Google Cloud SDK - command completion for gcloud
 #source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-
-# NVM 2021.07.21
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
